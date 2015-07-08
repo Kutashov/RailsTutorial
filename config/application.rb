e5400d3413fb518328c961c346176e8bb2941177
a.kutashov <kutashov.alexandr@yandex.ru>
2015-07-08 20:15:01 +0300

    Add lograge to project

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module RailsTutorial
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.lograge.enabled = true
+    config.lograge.ignore_actions = ['status#ping']
   end
 end
